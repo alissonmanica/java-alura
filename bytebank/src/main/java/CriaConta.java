5ca8bc553e492244eb1a075fe172b211a440e5dc
Alisson Manica <alissonmanica.aai@gmail.com>
2023-02-14 00:02:06 -0300

    feat(JavaAlura): created new files from Java POO

---
+++ bytebank/src/main/java/CriaConta.java
@@ -0,0 +1,21 @@
+public class CriaConta {
+    public static void main(String[] args) {
+        Conta primeiraConta = new Conta();
+        primeiraConta.saldo = 200;
+
+        primeiraConta.saldo += 100;
+
+        Conta segundaConta = new Conta();
+        segundaConta.saldo = 50;
+        System.out.println("na primeira conta tem " + primeiraConta.saldo);
+        System.out.println("na segunda conta tem " + segundaConta.saldo);
+
+        System.out.println(primeiraConta.agencia);
+        System.out.println(primeiraConta.numero);
+
+        System.out.println(segundaConta.agencia);
+
+        segundaConta.agencia = 146;
+        System.out.println("agora a segunda conta esta na agencia " + segundaConta.agencia);
+    }
+}
